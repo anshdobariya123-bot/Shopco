dd3b4b22ffba68842a97be0c3f959fbba3af3f47
Ansh Dobariya <anshdobariya123@gmail.com>
2025-12-15 15:49:46 +0530

    admin

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -8,9 +8,7 @@ const IMAGE_BASE =
 
 /* ================= HELPERS ================= */
 const getImageUrl = (img) =>
-  img
-    ? `${IMAGE_BASE}/uploads/${img.replace(/^\//, "")}`
-    : "/no-image.png";
+  img ? `${IMAGE_BASE}/${img.replace(/^\//, "")}` : "/no-image.png";
 
 /* ================= COMPONENT ================= */
 const ProductCard = ({ product }) => {
